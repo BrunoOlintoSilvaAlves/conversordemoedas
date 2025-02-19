a6853ab8d563d69b81946f2918a63ab906fd3606
brunoolintosilvaalves <4br1n0@gmail.com>
2025-02-19 20:30:45 -0300

    estrutura básica

--- cotacao.js
+++ cotacao.js
@@ -8,36 +8,34 @@ const input_to_ammount = document.getElementById('to_ammount');
 const tax_info = document.getElementById('tax_info');
 const swap = document.getElementById('swap');
 
-label_from_currency.addEventListener('change',calculate);
+label_from_currency.addEventListener('change', calculate);
 input_from_ammount.addEventListener('input', calculate);
-label_to_currency.addEventListener('change',calculate);
+label_to_currency.addEventListener('change', calculate);
 input_to_ammount.addEventListener('input', calculate);
 swap.addEventListener('click', infoSwap);
 
 main();
 
 function main(){
-    let currency = { "BRL": "Real, "EUR: "Euro",
-    "USD": "Dollar"};
+    let currency = {"BRL":"Real", "EUR": "Euro", "USD": "Dollar"};
     let options = [];
-    for(var[key, value] of Object.entries(currency)){
-        options.push(`<option value='${key}'>${value}</option>`);
-    }
+    for (var[key, value] of Object.entries(currency)){
+        options.push(`<option value='${key}'> ${value} </option>`);
     }
     label_from_currency.innerHTML = options.join('\n');
-    label_to_currency.innerHTML options.join('\n');
+    label_to_currency.innerHTML = options.join('\n');
     calculate();
 }
 
 function infoSwap(){
-    let temp= label_from_currency.value;
+    let temp = label_from_currency.value;
     label_from_currency.value = label_to_currency.value;
-    label_to_currency.value= temp;
-    calculate;
+    label_to_currency.value = temp;
+    calculate();
 }
 
 async function getURL(url){
-    return (await fetch(url)).json();
+    return(await fetch(url)).json();
 }
 
 function getInfoSelect(select){
@@ -47,11 +45,8 @@ function getInfoSelect(select){
 async function calculate(){
     let from = label_from_currency.value;
     let to = label_to_currency.value;
-    let { rates } = await
-getURL(`https://api.exchangerate-api.com/v4/latest/${from}`);
-let rate = rates[to];
-tax_info.innerText = `1
-${getInfoSelect(label_from_currency)} = ${rate}
-${getINfoSelect(label_to_currency)}`
+    let { rates } = await getURL(`https://api.exchangerate-api.com/v4/latest/${from}`);
+    let rate = rates[to];
+    tax_info.innerText = `1 ${getInfoSelect(label_from_currency)} = ${rate} ${getInfoSelect(label_to_currency)}`;
     input_to_ammount.value = (input_from_ammount.value * rate).toFixed(2);
 }
